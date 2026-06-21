dad352f6c5a8c2f26d651cd0fb85f9c7f52e43b2
navi <navinyakhandate8@gmail.com>
2026-06-21 21:50:34 +0530

    added project code

--- day1Task.py
+++ day1Task.py
@@ -6,18 +6,23 @@ class Student:
         self.section = section
         self.marks = marks
 
+    def display_report_card(self):
+        print("___Report Card___")
+        print("Name:", self.name)
+        print("Age:", self.age)
+        print("Grade:", self.grade)
+        print("Section:", self.section)
+        print("Marks:", self.marks)
+
     def update_grade(self, new_grade):
         self.grade = new_grade
 
 s1 = Student("raj", 15, 10, "A", {"maths": 85, "science": 90, "english": 80})
 s2 = Student("sita", 14, 9, "B", {"maths": 78, "science": 82, "english": 88})
 s3 = Student("gita", 16, 11, "A", {"maths": 92, "science": 95, "english": 89})
-print("___Report Card___")
-print("Name:", s1.name)
-print("Age:", s1.age)
-print("Grade:", s1.grade)
-print("Section:", s1.section)
-print("Marks:", s1.marks)
+s1.display_report_card()
+s2.display_report_card()
+s3.display_report_card()
 
 print("\n___Report Card___")
 print("Name:", s2.name)
