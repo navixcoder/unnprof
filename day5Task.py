014122dcaa4d251d2a2086ad6de0e3a3da7e7247
navi <navinyakhandate8@gmail.com>
2026-06-27 15:17:50 +0530

    extracted emails along with ph.no

--- day5Task.py
+++ day5Task.py
@@ -1,13 +1,11 @@
 import re
 try:
     with open("fintechDoc.txt","r") as f:
-        email_pattern = r"[a-zA-Z0-9_.]+@""[a-z.]+"
-        number_pattern = r"[\d{0-9}]+""[+\d{2}-\d{5}-\d{5}+""\d{4}-\d{3}-\d{4}]+""[+\d{2} \d{3} \d{5}]"
+        email_pattern = r'[\w\.]+@[a-z]+\.''[a-z]+'
         Data=(f.read())
         print(Data)
         emails = re.findall(email_pattern,Data)
         print(emails)
-        phone_numbers = re.findall(number_pattern,Data)
 
 except FileNotFoundError :
     print('file does not Exists')
